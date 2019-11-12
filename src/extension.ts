a8f62902dc2af1b144cae37b1301c2796eda0992
sharklasers996 <sharklasers996@gmail.com>
2019-11-12 20:37:12 +0200

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,38 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let jumpDown5 = vscode.commands.registerCommand('userFunctions.jumpDown5', () => {
+		for (let i = 0; i < 5; i++) {
+			vscode.commands.executeCommand('cursorMove', { to: 'down', by: 'line' });
+		}
+	});
+
+	context.subscriptions.push(jumpDown5);
+
+	let jumpUp5 = vscode.commands.registerCommand('userFunctions.jumpUp5', () => {
+		for (let i = 0; i < 5; i++) {
+			vscode.commands.executeCommand('cursorMove', { to: 'up', by: 'line' });
+		}
+	});
+
+	context.subscriptions.push(jumpUp5);
+
+	let scrollUp = vscode.commands.registerCommand('userFunctions.scrollUp', () => {
+		for (let i = 0; i < 5; i++) {
+			vscode.commands.executeCommand('editorScroll', { to: 'up', by: 'line' });
+		}
+	});
+
+	context.subscriptions.push(scrollUp);
+
+	let scrollDown = vscode.commands.registerCommand('userFunctions.scrollDown', () => {
+		for (let i = 0; i < 5; i++) {
+			vscode.commands.executeCommand('editorScroll', { to: 'down', by: 'line' });
+		}
+	});
+
+	context.subscriptions.push(scrollDown);
+}
+
+export function deactivate() { }
