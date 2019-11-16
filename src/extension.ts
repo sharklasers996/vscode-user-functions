8b14464337ae7d5287ebd9f66a262e8ebee6bdf4
sharklasers996 <sharklasers996@gmail.com>
2019-11-16 12:55:51 +0200

    Switched cursor commands to emacs

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let jumpDown5 = vscode.commands.registerCommand('userFunctions.jumpDown5', () => {
 		for (let i = 0; i < 5; i++) {
-			vscode.commands.executeCommand('cursorMove', { to: 'down', by: 'line' });
+			vscode.commands.executeCommand('emacs.cursorDown');
 		}
 	});
 
@@ -12,7 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let jumpUp5 = vscode.commands.registerCommand('userFunctions.jumpUp5', () => {
 		for (let i = 0; i < 5; i++) {
-			vscode.commands.executeCommand('cursorMove', { to: 'up', by: 'line' });
+			vscode.commands.executeCommand('emacs.cursorUp');
 		}
 	});
 
